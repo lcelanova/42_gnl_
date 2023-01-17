ecb9beab7b4884775554d03bbcd209f586ac1dda
lcelanova <lmunofer@gmail.com>
2023-01-17 17:17:17 +0100

    chagnes

--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -21,5 +21,7 @@ char    *get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *s1);
 char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
 #endif
