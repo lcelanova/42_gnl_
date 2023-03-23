61e932588fa7a51ff825a5b252c98e311a0dc420
lcelanova <lmunofer@gmail.com>
2023-03-23 18:34:20 +0100

    buffer size modified

--- GNL/get_next_line_bonus.h
+++ GNL/get_next_line_bonus.h
@@ -12,20 +12,25 @@
 
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
+
 # include <string.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdarg.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
 char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *s1);
 char	*ft_strchr(const char *s);
-size_t	ft_strlen(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	*join_and_free(char *text, char *buffer, int bytes_read);
 char	*copy_and_free(char *text, int i);
 int		find_n(char *string);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
 #endif
